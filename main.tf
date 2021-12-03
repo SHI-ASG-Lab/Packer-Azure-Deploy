33acca0561dcb830b1338472264d822870c57684
j-isley <79328510+j-isley@users.noreply.github.com>
2021-12-03 09:55:56 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,27 +10,27 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.TF_VAR_ARM_SUBSCRIPTION_ID
-  client_id       = var.TF_VAR_ARM_CLIENT_ID
-  client_secret   = var.TF_VAR_ARM_CLIENT_SECRET
-  tenant_id       = var.TF_VAR_ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 # Variable Declarations
 
-variable "TF_VAR_ARM_CLIENT_ID" {
+variable "ARM_CLIENT_ID" {
   type = string
   sensitive = true
 }
-variable "TF_VAR_ARM_CLIENT_SECRET" {
+variable "ARM_CLIENT_SECRET" {
   type = string
   sensitive = true
 }
-variable "TF_VAR_ARM_SUBSCRIPTION_ID" {
+variable "ARM_SUBSCRIPTION_ID" {
   type = string
   sensitive = true
 }
-variable "TF_VAR_ARM_TENANT_ID" {
+variable "ARM_TENANT_ID" {
   type = string
   sensitive = true
 }
