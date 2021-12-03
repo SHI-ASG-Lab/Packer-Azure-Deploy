500f24c82a359a9a8376998f9dc9985d5b545d3f
j-isley <79328510+j-isley@users.noreply.github.com>
2021-12-02 21:31:01 -0600

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,10 @@
+RG_SP_Name          = "Lab"
+RG_Env_Tag          = "Dev"
+Requestor           = "KBormann"
+Owner               = "KBormann"
+
+ExistingImageName   = "Pkr_Ubuntu-East-2021.12.02.1621"
+
+VmName              = "Ubuntu-EastUS-12.3.21"
+
+NumUbuntu           = 1
