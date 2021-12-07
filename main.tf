e6a5db6d7d25d2624a78982e30f0566cbfa4efc7
thorfinnKB <74472882+thorfinnKB@users.noreply.github.com>
2021-12-07 10:27:06 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = data.azurerm_subnet.default.id
     private_ip_address_allocation = "static"
-    private_ip_address            = "10.28.0.6"
+    private_ip_address            = "10.28.0.10"
     # private_ip_address            = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.azPubIp.id
     primary                       = true
